2db685c2779a6aa1e149e479d9fe9e4a3a93e40e
sherllo <sherllochen@gmail.com>
2020-06-16 12:23:17 +0800

    整合

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const fs = require('fs');
 const files = require('./lib/files');
 const clear = require('clear');
 const chalk = require('chalk');
 const figlet = require('figlet');
-const md5File = require('md5-file');
 
 clear();
 console.log(
@@ -11,25 +9,39 @@ console.log(
         figlet.textSync('Automatic Utilies', { horizontalLayout: 'full' })
     )
 )
-// 获取当前目录
-// const rootPath = process.cwd();
-const rootPath = "D:\\南阳报纸审核目录\\扫描\\测试脚本";
-console.log(`当前操作目录是 ${rootPath}`);
+
+inquirer
+  .prompt([
+    {
+      type: "checkbox",
+      message: "请选择所需功能",
+      name: "selected",
+      choices: [
+        {
+          name: "构建临时审核目录",
+          checked: true,
+        },
+        {
+          name: "拆分到原目录结构",
+        },
+      ],
+      validate: function (answer) {
+        if (answer.length < 1) {
+          return "You must choose at least one topping."
+        }
+
+        return true
+      },
+    },
+  ])
+  .then((answers) => {
+    console.log(JSON.stringify(answers, null, "  "))
+    let selected = answers["selected"][0]
+    console.log(selected)
+  })
 
 // 询问用户做何操作 1是移动到审核目录，2是恢复
 
 // 用户若选择移动到审核目录，则提示用户检查是否01首图是封面，02首图是封底。
 
-// 存储文件hash
-files.cacheFilesHash(rootPath);
 
-// todo 后续如何查询？读取整个文件到内存的hash中，可以为文件hash，value为path
-
-// 创建目录
-files.createTempOperationDir(rootPath);
-
-// 移动文件
-files.moveFilesToTempDir(rootPath);
-
-// 重命名文件
-files.batchRename(rootPath);
---
+++ test.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const files = require("./lib/files")
+const clear = require("clear")
+const chalk = require("chalk")
+const figlet = require("figlet")
+const inquirer = require("inquirer")
+
+clear()
+console.log(
+  chalk.yellow(figlet.textSync("Only for test", { horizontalLayout: "full" }))
+)
+
+let questions = [
+  {
+    type: "confirm",
+    name: "confirmed",
+    message: "请确认当前文件夹？确认请输入 y 然后按Enter。",
+    default: false,
+  },
+]
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers, null, "  "))
+  if (answers["confirmed"]) {
+    
+  } else {
+    console.log("请将本程序复制到正确文件夹后再运行一遍")
+  }
+})
